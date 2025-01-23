92cceed89a39e46796378d1fdad0ed2273ab6b13
Ogglord <oag@proton.me>
2025-01-24 00:31:41 +0100

    test 3

--- scripts/distrobox-shims.sh
+++ scripts/distrobox-shims.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-ln -fs /bin/sh /usr/bin/sh
+ln -fs /bin/bash /usr/bin/sh
 ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/docker
 ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/flatpak
 ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/podman
-#ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/rpm-ostree
-#ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/transactional-update
+ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/rpm-ostree
+ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/transactional-update
