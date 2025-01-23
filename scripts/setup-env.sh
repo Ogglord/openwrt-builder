c2f214f4195002749937b1c3d3f51de8b8802e80
Ogglord <oag@proton.me>
2025-01-23 23:12:21 +0100

    moved llvm install to setup-env.sh

--- scripts/setup-env.sh
+++ scripts/setup-env.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+
+GO_VERSION=${GO_VERSION:-1.22.5}
+
 echo "Running setup-env.sh..."
 
 cat <<EOF > /etc/apt/sources.list
@@ -15,9 +18,16 @@ apt-get update -y
 grep -v '^#' ./openwrt-builder.packages | xargs apt-get install -y 
 
 ## Install GO
+echo "Installing GO $GO_VERSION..."
 wget --ca-directory=/etc/ssl/certs/ https://go.dev/dl/go${GO_VERSION}.linux-arm64.tar.gz
 tar -C /usr/local -xzf go${GO_VERSION}.linux-arm64.tar.gz \
 rm -f go${GO_VERSION}.linux-arm64.tar.gz
+
+## Install LLVM
+echo "Installing latest llvm toolchain..."
+bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)"
+llvm_host_path="/usr/lib/$(ls /usr/lib/ | grep llvm | sort -r | head -1 | cut -d' ' -f11)" \
+    && echo "export LLVM_HOST_PATH=$llvm_host_path" >> /etc/bash.bashrc
 
 # Symlink distrobox shims
 ./distrobox-shims.sh
