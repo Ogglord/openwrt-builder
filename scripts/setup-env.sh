3b318c6a8642eeb2b4cf6ac51464d6fb64f0f1c7
Ogglord <oag@proton.me>
2025-01-23 22:50:34 +0100

    attempt 4

--- scripts/setup-env.sh
+++ scripts/setup-env.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
 echo "Running setup-env.sh..."
 
-# Symlink distrobox shims
-./distrobox-shims.sh
+cat <<EOF > /etc/apt/sources.list
+deb http://archive.ubuntu.com/ubuntu noble main restricted universe multiverse
+deb http://archive.ubuntu.com/ubuntu noble-updates main restricted universe multiverse
+deb http://archive.ubuntu.com/ubuntu noble-backports main restricted universe multiverse
+deb http://security.ubuntu.com/ubuntu noble-security main restricted universe multiverse
+EOF
+
+
 
 # Update the container and install packages
-apt update
-grep -v '^#' ./openwrt-builder.packages | xargs apt install -y 
+apt-get update -y
+grep -v '^#' ./openwrt-builder.packages | xargs apt-get install -y 
 
 # Clean up unnecessary files to reduce image size
-apt clean && rm -rf /var/lib/apt/lists/*
+apt-get clean && rm -rf /var/lib/apt/lists/*
+
+# Symlink distrobox shims
+./distrobox-shims.sh
