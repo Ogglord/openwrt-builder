a9518507db158640765ae769dbe93604ddb9d39f
Ogglord <oag@proton.me>
2025-01-25 21:39:52 +0100

    simplified motd

--- shell/config.fish
+++ shell/config.fish
@@ -21,4 +21,24 @@ if status is-interactive
             set gitprompt (fish_git_prompt)
             echo "📦[$USER@$CONTAINER_ID $current_dir] $gitprompt> "
     end
+
+    function fish_greeting
+        set_color --bold green
+        echo "Welcome!"
+        set_color normal
+        echo "--"
+        set_color --bold yellow
+        echo -n "Use ´buildo´"
+        set_color normal
+        echo " to invoke the OpenWrt build helper"
+        set_color --bold yellow
+        echo -n "Use ´llvm-fix´"
+        set_color normal
+        echo " to update the .config to use the LLVM toolchain"
+        echo "--"
+        set_color --bold blue
+        echo "Note: Both tools must be run from root of the source folder"
+        set_color normal
+        echo "###################################################################"
+    end
 end
